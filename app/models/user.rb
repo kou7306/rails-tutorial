abe3cd63c0e576442c6941e1179d6f49657001a3
kou7306 <kouta.1206.y@icloud.com>
2024-03-06 11:06:44 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,11 @@ class User < ApplicationRecord
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    if ActiveModel::SecurePassword.min_cost
+      cost = BCrypt::Engine::MIN_COST
+    else
+      cost = BCrypt::Engine.cost
+    end
     BCrypt::Password.create(string, cost: cost)
   end
 
