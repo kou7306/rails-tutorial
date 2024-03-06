c08192ed995ead55c6d8742be08fa7751dac8738
kou7306 <kouta.1206.y@icloud.com>
2024-03-06 11:00:48 +0900

    10章

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_04_032125) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_06_014711) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_04_032125) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
