6b7658df69bd1be5921622767733ef89bc5e5771
kou7306 <kouta.1206.y@icloud.com>
2024-03-09 13:06:48 +0900

    chapter12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_06_021538) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_08_145420) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_06_021538) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
