89bca43cc5c6ef12a0f07f4ea9757b56f6c2f3cc
kou7306 <kouta.1206.y@icloud.com>
2024-03-13 18:12:55 +0900

    fix

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "kouta.131206@gmail.com"
+  layout "mailer"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,38 @@
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+# 追加のユーザーをまとめて生成する
+user_data = []
+password_digest = User.digest('password')
+99.times do |i|
+  user_data << {
+    name: Faker::Name.name,
+    email: "example-#{i+1}@railstutorial.org",
+    password_digest: password_digest,
+    activated: true,
+    activated_at: Time.zone.now
+  }
+end
+
+User.insert_all!(user_data)
+
+# ユーザーの一部を対象にマイクロポストを生成する
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# ユーザーフォローのリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
